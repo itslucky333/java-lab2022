122231db69d8f20a4221e65183b564ebeceb616b
laxmi kanta poudel <justkantapoudel@gmail.com>
2022-11-10 09:12:29 +0545

    commiting_few_programs

---
+++ javaLabByMe/Lab1/CalculateSum.java
@@ -0,0 +1,9 @@
+class CalculateSum{
+    public static void main(String args[]){
+      int num1, num2, sum;
+      num1=100;
+      num2=200;
+      sum=num1+num2;
+      System.out.println("the result of sum is " + sum);  
+    }
+}
