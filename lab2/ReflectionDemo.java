c75db07fa61943bf2305d306acb4266308b76d1f
bidur <bidur.devkota@gmail.com>
2022-11-14 22:58:12 +0545

    lab 2 files added

---
+++ lab2/ReflectionDemo.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.*;
+public class ReflectionDemo {
+      public static void main(String args[])
+      {
+         try {
+            Class c = Class.forName("java.util.Stack");
+            Method methods[] = c.getDeclaredMethods();
+            
+            // dump all methods in the Stack class
+            for (int i = 0; i < methods.length; i++)
+                        System.out.println(methods[i].toString());
+         }
+         catch (Exception e) {
+            System.err.println(e);
+         }
+      }
+   }
+
+
---
+++ lab2/package/0_readme.txt
@@ -0,0 +1,5 @@
+
+Package name: mypackage
+Class inside package: Food.java, Animal.java
+
+Classes which use the package:  Vegetables.java , PackageDemo.java , Fruits.java
